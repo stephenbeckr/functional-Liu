5eedfc8cc23b740f8a92e88c8891f1ffe5673086
Stephen Becker <stephen.beckr@gmail.com>
2026-03-13 15:15:41 -0600

    Add LOO CV option

--- fliu.py
+++ fliu.py
@@ -26,37 +26,47 @@ def mse(y, yhat):
 # =========================
 # OLS
 # =========================
-def ols(X,y):
+def ols(X,y, criteria = 'GCV'):
     """ Ordinary least squares
     Input: X, y
         Design matrix X (n x p), response y (size n)
-        Optional: optimal lambda parameters
+        Optional: optimal lambda parameters,
+            criteria is either 'GCV' for the Generalized Cross Validation criteria,
+            or 'CV' to use leave-one-out (LOO) Cross Valiation
 
     Output: beta_optimal, gcv
-        where gcv is the generalized cross validation score.
+        where gcv is the generalized cross validation score
+        or the Leave-One-Out (LOO) cross-validation score
     """
     n, p = X.shape
     beta = sla.lstsq(X,y)[0]
     yhat = X @ beta
     P    = X @ sla.lstsq(X,np.eye(n))[0]
-    gcv_ols = mse(y,yhat)/( (1-np.trace(P)/n)**2 )
+    if criteria.lower() == 'gcv':
+        gcv_ols = mse(y,yhat)/( (1-np.trace(P)/n)**2 )
+    elif criteria.lower() == 'cv':
+        gcv_ols = np.mean(((y-yhat)/(1-np.diag(P)))**2 ) 
+    else:
+        raise ValueError('Criteria must be either "GCV" or "CV"')
     return beta, gcv_ols
 
 # =========================
 # RIDGE GCV.
 # =========================
-def ridge(X, y, lam_opt = None, lambda_bounds = (1e-6,1e6), opt_method = 'SLSQP'):
+def ridge(X, y, lam_opt = None, lambda_bounds = (1e-6,1e6), opt_method = 'SLSQP', criteria = 'GCV'):
     """ Classic ridge regression: $beta = (XX^T + lambda I)^{-1} X^T y$ 
     Input: X, y, [lam_opt]
         Design matrix X (n x p), response y (size n)
         Optional: optimal lambda parameters. If not provided, then optimal values
-            (based on the GCV criteria) are found using a gradient-based
+            (based on the GCV or LOO CV criteria) are found using a gradient-based
             optimization solver
             In this case, lambda_bounds are bound constraints on the size of lambda
             and opt_method is a string controlling which solver to use from scipy.optimize.minimize
+            criteria is either 'GCV' for the Generalized Cross Validation criteria,
+            or 'CV' to use leave-one-out (LOO) Cross Valiation
 
     Output: beta_optimal, gcv, lambda_optimal
-        where gcv is the generalized cross validation score.
+        where gcv is the generalized cross validation score (or LOO CV).
     """
     n, p = X.shape
     G  = jnp.asarray(X.T @ X)
@@ -64,6 +74,7 @@ def ridge(X, y, lam_opt = None, lambda_bounds = (1e-6,1e6), opt_method = 'SLSQP'
     X  = jnp.asarray(X)
 
     def gcv(lam_vec):
+        """ Generalized Cross Validation criterion """
         lam = lam_vec[0]
         A = G + lam*jnp.eye(p)
         (c,lower)  = jsla.cho_factor(A)
@@ -72,24 +83,42 @@ def ridge(X, y, lam_opt = None, lambda_bounds = (1e-6,1e6), opt_method = 'SLSQP'
         P      = X @ jsla.cho_solve( (c,lower), X.T )
         traceP = jnp.trace(P)
         return jnp.mean((jnp.asarray(y)-yhat)**2)/((1-traceP/n)**2)
+    
+    def cv(lam_vec):
+        """ Leave-one-out Cross Validation criterion, specialized for linear predictors """
+        lam = lam_vec[0]
+        A = G + lam*jnp.eye(p)
+        (c,lower)  = jsla.cho_factor(A)
+        beta = jsla.cho_solve( (c,lower), Xy )
+        yhat = jnp.asarray(X) @ beta
+        P      = X @ jsla.cho_solve( (c,lower), X.T )
+        return jnp.mean(((jnp.asarray(y)-yhat)/(1-jnp.diag(P)))**2 ) # only difference from GCV
+    
+    if criteria.lower() == 'gcv':
+        crit = gcv
+    elif criteria.lower() == 'cv':
+        crit = cv
+    else:
+        raise ValueError('Criteria must be either "GCV" or "CV"')
+    
     if lam_opt is None:
         # We look for the optimal vector
-        grad      = jax.jit( jax.grad(gcv) )
-        results   = opt.minimize(gcv, np.array([1.0]), jac=grad,
+        grad      = jax.jit( jax.grad(crit) )
+        results   = opt.minimize(crit, np.array([1.0]), jac=grad,
                           bounds=[lambda_bounds], method=opt_method)
         lam_opt   = results.x[0]
         beta      = sla.solve(X.T@X + lam_opt*np.eye(p), X.T@y,assume_a='pos')
         gcv_ridge = results.fun # already computed
     else:
         beta = sla.solve(X.T@X + lam_opt*np.eye(p), X.T@y,assume_a='pos')
-        gcv_ridge = gcv(lam_opt)
+        gcv_ridge = crit(lam_opt)
     return beta, gcv_ridge, lam_opt
 
 
 # =========================
 # CLASSICAL LIU GCV
 # =========================
-def classical_liu(X, y, params_optimal = None, lambda_bounds = (1e-6,1e6), opt_method = 'SLSQP'):
+def classical_liu(X, y, params_optimal = None, lambda_bounds = (1e-6,1e6), opt_method = 'SLSQP', criteria = 'GCV'):
     """ Standard liu: $beta = (XX^T + lambda I)^{-1}(X^T y + d lambda beta_{OLS})$
     Input: X, y, [params_optimal]
         Design matrix X (n x p), response y (size n)
@@ -99,9 +128,11 @@ def classical_liu(X, y, params_optimal = None, lambda_bounds = (1e-6,1e6), opt_m
             In this case, lambda_bounds are bound constraints on the size of lambda
             (the value of d is always constrained to be in (0,1))
             and opt_method is a string controlling which solver to use from scipy.optimize.minimize
+            criteria is either 'GCV' for the Generalized Cross Validation criteria,
+            or 'CV' to use leave-one-out (LOO) Cross Valiation
 
     Output: beta_optimal, gcv, lambda_optimal, d_optimal
-        where gcv is the generalized cross validation score.
+        where gcv is the generalized cross validation score (or LOO CV).
     """
     n, p = X.shape
     G  = jnp.asarray(X.T @ X)
@@ -120,16 +151,32 @@ def classical_liu(X, y, params_optimal = None, lambda_bounds = (1e-6,1e6), opt_m
         traceP = jnp.trace(P)
         return jnp.mean((jnp.asarray(y)-yhat)**2)/((1-traceP/n)**2)
 
+    def cv(params):
+        lam, d = params
+        A = G + lam*jnp.eye(p)
+        (c,lower)  = jsla.cho_factor(A)
+        beta = jsla.cho_solve((c,lower), Xy + d*lam*beta_OLS)
+        yhat = X @ beta
+        P    = X @ jsla.cho_solve( (c,lower), X.T + d*lam*P_OLS )
+        return jnp.mean(((jnp.asarray(y)-yhat)/(1-jnp.diag(P)))**2 ) # only difference from GCV
+    
+    if criteria.lower() == 'gcv':
+        crit = gcv
+    elif criteria.lower() == 'cv':
+        crit = cv
+    else:
+        raise ValueError('Criteria must be either "GCV" or "CV"')
+    
     if params_optimal is None:
-        grad = jax.jit( jax.grad(gcv) )
-        res = opt.minimize(gcv, np.array([1.0,0.5]), jac=grad,
+        grad = jax.jit( jax.grad(crit) )
+        res = opt.minimize(crit, np.array([1.0,0.5]), jac=grad,
                           bounds=[lambda_bounds,(0,1)], method=opt_method)
 
         lam_opt, d_opt = res.x
         gcv_liu = res.fun
     else:
         lam_opt, d_opt = params_optimal
-        gcv_liu = gcv(params_optimal)
+        gcv_liu = crit(params_optimal)
 
     beta = sla.solve(X.T@X + lam_opt*np.eye(p),
                      X.T@y + d_opt*lam_opt*np.array(beta_OLS),
@@ -140,7 +187,7 @@ def classical_liu(X, y, params_optimal = None, lambda_bounds = (1e-6,1e6), opt_m
 # =========================
 # CARDOT'S ESTIMATOR
 # =========================
-def cardot(X, y, R, params_optimal=None, lambda_bounds = (1e-6,1e6), opt_method = 'SLSQP'):
+def cardot(X, y, R, params_optimal=None, lambda_bounds = (1e-6,1e6), opt_method = 'SLSQP', criteria = 'GCV'):
     """ Cardot's generalized ridge regression estimator
         $beta = (XX^T + Q)^{-1} X^T y$
         where Q = lambda(alpha I + (1-alpha) R)
@@ -152,10 +199,12 @@ def cardot(X, y, R, params_optimal=None, lambda_bounds = (1e-6,1e6), opt_method 
             In this case, lambda_bounds are bound constraints on the size of lambda
             (the value of alpha is always constrained to be in (0,1))
             and opt_method is a string controlling which solver to use from scipy.optimize.minimize
+            criteria is either 'GCV' for the Generalized Cross Validation criteria,
+            or 'CV' to use leave-one-out (LOO) Cross Valiation
 
     Output: beta_optimal, gcv, lambda_optimal, alpha_optimal
-        where gcv is the generalized cross validation score
-        and lambda and alpha were determined to minimize gcv.
+        where gcv is the generalized cross validation score (or LOO CV)
+        and lambda and alpha were determined to minimize gcv (or LOO CV).
     """
     n, p = X.shape
     G  = jnp.asarray(X.T @ X)
@@ -174,16 +223,34 @@ def cardot(X, y, R, params_optimal=None, lambda_bounds = (1e-6,1e6), opt_method 
         P    = X @ jsla.cho_solve( (c,lower), X.T )
         traceP = jnp.trace(P)
         return jnp.mean((jnp.asarray(y)-yhat)**2)/((1-traceP/n)**2)
+
+    def cv(params):
+        lam, alpha = params
+        Q = lam*(alpha*I + (1-alpha)*Rj)
+        A = G + Q
+        (c,lower)  = jsla.cho_factor(A)
+        beta = jsla.cho_solve((c,lower), Xy)
+        yhat = X @ beta
+        P    = X @ jsla.cho_solve( (c,lower), X.T )
+        return jnp.mean(((jnp.asarray(y)-yhat)/(1-jnp.diag(P)))**2 ) # only difference from GCV
+    
+    if criteria.lower() == 'gcv':
+        crit = gcv
+    elif criteria.lower() == 'cv':
+        crit = cv
+    else:
+        raise ValueError('Criteria must be either "GCV" or "CV"')
+    
     if params_optimal is None:
-        grad = jax.jit( jax.grad(gcv) )
-        res = opt.minimize(gcv, np.array([1.0,0.5]), jac=grad,
+        grad = jax.jit( jax.grad(crit) )
+        res = opt.minimize(crit, np.array([1.0,0.5]), jac=grad,
                           bounds=[lambda_bounds,(0,1)], method=opt_method)
 
         lam_opt, alpha_opt = res.x
         gcv_cardot = res.fun
     else:
         lam_opt, alpha_opt = params_optimal
-        gcv_cardot = gcv(params_optimal)
+        gcv_cardot = crit(params_optimal)
 
     Q = lam_opt*(alpha_opt*np.eye(p) + (1-alpha_opt)*R)
     beta = sla.solve(X.T@X + Q, X.T@y, assume_a = 'pos')
@@ -194,7 +261,7 @@ def cardot(X, y, R, params_optimal=None, lambda_bounds = (1e-6,1e6), opt_method 
 # =========================
 # FUNCTIONAL LIU GCV
 # =========================
-def functional_liu(X, y, R, params_optimal=None, lambda_bounds = (1e-6,1e6), opt_method = 'SLSQP'):
+def functional_liu(X, y, R, params_optimal=None, lambda_bounds = (1e-6,1e6), opt_method = 'SLSQP', criteria = 'GCV'):
     """ Functional Liu estimator: combination of Cardot's generalized ridge with Liu's biased estimator
         $(XX^T + Q)^{-1}(X^T y + d lambda Q beta_{OLS})$
         where Q = lambda(alpha I + (1-alpha) R) as in Cardot.
@@ -206,10 +273,12 @@ def functional_liu(X, y, R, params_optimal=None, lambda_bounds = (1e-6,1e6), opt
             In this case, lambda_bounds are bound constraints on the size of lambda
             (the value of d and alpha are always constrained to be in (0,1))
             and opt_method is a string controlling which solver to use from scipy.optimize.minimize
+            criteria is either 'GCV' for the Generalized Cross Validation criteria,
+            or 'CV' to use leave-one-out (LOO) Cross Valiation
 
     Output: beta_optimal, gcv, lambda_optimal, d_optimal, alpha_optimal
-        where gcv is the generalized cross validation score
-        and lambda, d and alpha were determined to minimize gcv.
+        where gcv is the generalized cross validation score (or LOO CV)
+        and lambda, d and alpha were determined to minimize gcv (or LOO CV).
     """
     n, p = X.shape
     G  = jnp.asarray(X.T @ X)
@@ -230,17 +299,35 @@ def functional_liu(X, y, R, params_optimal=None, lambda_bounds = (1e-6,1e6), opt
         P    = X @ jsla.cho_solve( (c,lower), X.T + d*Q@P_OLS )
         traceP = jnp.trace(P)
         return jnp.mean((jnp.asarray(y)-yhat)**2)/((1-traceP/n)**2)
+    
+    def cv(params):
+        lam, d, alpha = params
+        Q = lam*(alpha*I + (1-alpha)*Rj)
+        A = G + Q
+        (c,lower)  = jsla.cho_factor(A)
+        beta = jsla.cho_solve((c,lower), Xy + d*(Q@beta_OLS) )
+        yhat = X @ beta
+        P    = X @ jsla.cho_solve( (c,lower), X.T + d*Q@P_OLS )
+        return jnp.mean(((jnp.asarray(y)-yhat)/(1-jnp.diag(P)))**2 ) # only difference from GCV
+    
+    if criteria.lower() == 'gcv':
+        crit = gcv
+    elif criteria.lower() == 'cv':
+        crit = cv
+    else:
+        raise ValueError('Criteria must be either "GCV" or "CV"')
+    
     if params_optimal is None:
-        grad = jax.jit( jax.grad(gcv) )
+        grad = jax.jit( jax.grad(crit) )
 
-        res = opt.minimize(gcv, np.array([1.0,0.5,0.5]), jac=grad,
+        res = opt.minimize(crit, np.array([1.0,0.5,0.5]), jac=grad,
                           bounds=[lambda_bounds,(0,1),(0,1)], method=opt_method)
 
         lam_opt, d_opt, alpha_opt = res.x
         gcv_fliu = res.fun
     else:
         lam_opt, d_opt, alpha_opt = params_optimal
-        gcv_fliu = gcv(params_optimal)
+        gcv_fliu = crit(params_optimal)
 
     Q = lam_opt*(alpha_opt*np.eye(p) + (1-alpha_opt)*R)
     beta = sla.solve(X.T@X + Q,
